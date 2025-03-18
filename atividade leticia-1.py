aa131d35b12aa7c0b9962e031c645c4e39f0d6d2
IzaqueMarioJunior <izaque.junior23@etepd.com>
2025-03-18 14:28:32 -0300

    Add files via upload

---
+++ atividade leticia-1.py
@@ -0,0 +1,8 @@
+nome = input("") 
+idade = 15
+poder = "amizade"
+missao = "derrotar o vilao"
+vilao = "miguel"
+poder_vilao = "volei"
+
+print(f"e heroi {nome} com idade {idade} anos, esta e uma missao de {missao}, com os seus poderes de {poder}, ele ira derrotar o vilao {vilao} contra seu poder de {poder_vilao}.")
