7ac88d302173afeced86ff971124711c71c7cd29
cleythonsantos <cleythondossantos09@gmail.com>
2025-03-19 15:41:13 -0300

    editei

--- atividade leticia-1.py
+++ atividade leticia-1.py
@@ -4,5 +4,6 @@ poder = "amizade"
 missao = "derrotar o vilao"
 vilao = "miguel"
 poder_vilao = "volei"
+izaque = "izaque"
 
-print(f"e heroi {nome} com idade {idade} anos, esta e uma missao de {missao}, com os seus poderes de {poder}, ele ira derrotar o vilao {vilao} contra seu poder de {poder_vilao}.")
+print(f"o heroi {nome} com idade {idade} anos, esta em uma missao de {missao}, com os seus poderes de {poder}, ele ira derrotar o vilao {vilao} contra seu poder de {poder_vilao}. Até que {izaque} aparece e impede a bola de cair no chão, morrendo em seguida com um buraco na barriga. {nome} revoltado com a situaçao, deu uma pisa em {vilao}.")
